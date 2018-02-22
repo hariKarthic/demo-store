b66d99844f834dd41869d11d34aee0ac897bd44a
Vinci Rufus <vinci.rufus@gmail.com>
2018-02-22 11:03:55 -0600

    homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, CardGrid } from './fusion-components';
+import theme from './theme';
+import { ThemeProvider } from 'theming';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Nitro React App!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Navbar />
+
+          <CardGrid />
+        </div>
+      </ThemeProvider>
     );
   }
 }
